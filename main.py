c775c23670b396fb6638f32fef13dcce9b152141
Igorexa42 <101439313+Igorexa42@users.noreply.github.com>
2024-02-29 23:07:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import plotext as plt
 import psutil
 
 def main():
+	plt.clt()
+	plt.cld()
 	names = ["CPU", "RAM"]
-	percentages = [psutil.cpu_percent(), psutil.virtual_memory().available, ]
+	percentages = [psutil.cpu_percent(), psutil.virtual_memory().available]
 	plt.bar(names, percentages)
 	plt.title("Resource Control")
 	plt.show()
